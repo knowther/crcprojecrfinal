d910bb5d090ae6791f79c2017ba99822d6b0144d
Knowther <parentejohnny9@gmail.com>
2021-01-31 00:34:25 -0300

    Toda Estrutura inicial num JFrame

---
+++ src/bean/Estado.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+/**
+ *
+ * @author johnn
+ */
+@Entity
+@Table(name = "estado", catalog = "clinica_crc", schema = "")
+public class Estado implements Serializable {
+
+    @Transient
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "idestado")
+    private Integer idestado;
+    @Column(name = "nome")
+    private String nome;
+
+    
+    @OneToMany(mappedBy = "estado")
+    private List<Cidade> cidades = new ArrayList<>();
+    
+    @OneToMany(mappedBy = "estado")
+    private List<Paciente> pacientes = new ArrayList<>();
+    
+    public Estado() {
+    }
+
+    public Estado(Integer idestado) {
+        this.idestado = idestado;
+    }
+
+    public Integer getIdestado() {
+        return idestado;
+    }
+
+    public void setIdestado(Integer idestado) {
+        Integer oldIdestado = this.idestado;
+        this.idestado = idestado;
+        changeSupport.firePropertyChange("idestado", oldIdestado, idestado);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        String oldNome = this.nome;
+        this.nome = nome;
+        changeSupport.firePropertyChange("nome", oldNome, nome);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idestado != null ? idestado.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Estado)) {
+            return false;
+        }
+        Estado other = (Estado) object;
+        if ((this.idestado == null && other.idestado != null) || (this.idestado != null && !this.idestado.equals(other.idestado))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
+    }
+
+    public List<Cidade> getCidades() {
+        return cidades;
+    }
+
+    public void setCidades(List<Cidade> cidades) {
+        this.cidades = cidades;
+    }
+
+    public List<Paciente> getPacientes() {
+        return pacientes;
+    }
+
+    public void setPacientes(List<Paciente> pacientes) {
+        this.pacientes = pacientes;
+    }
+    
+}
---
+++ src/bean/Paciente.java
@@ -0,0 +1,287 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import bean.Cidade;
+import bean.Estado;
+import bean.Medico;
+import bean.Profissao;
+import bean.Turno;
+
+/**
+ *
+ * @author johnn
+ */
+@Entity
+@Table(name = "paciente", catalog = "clinica_crc", schema = "")
+public class Paciente implements Serializable {
+
+    @Transient
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idpaciente")
+    private Integer idpaciente;
+    @Column(name = "bairro")
+    private String bairro;
+    @Column(name = "cep")
+    private String cep;
+    @Column(name = "cpf")
+    private String cpf;
+    @Column(name = "dataNascimento")
+    @Temporal(TemporalType.DATE)
+    private Date dataNascimento;
+    @Column(name = "foto")
+    private String foto;
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "numero")
+    private String numero;
+    @Column(name = "rua")
+    private String rua;
+    @Column(name = "apelido")
+    private String apelido;
+  
+    @ManyToOne
+    private Cidade cidade;
+    
+    @ManyToOne
+    private Estado estado;
+    
+    @ManyToOne
+    private Profissao profissao;
+    
+    @ManyToOne
+    private Turno turno;
+
+    @ManyToOne
+    private Medico medico;
+    
+  
+   
+    
+    public Paciente() {
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+       Medico oldMedico = this.medico;
+        this.medico = medico;
+      changeSupport.firePropertyChange("medico", oldMedico, medico);
+    }
+    
+ public Turno getTurno() {
+        return turno;
+    }
+
+    public void setTurno(Turno turno) {
+         Turno oldTurno = this.turno;
+        this.turno = turno;
+        changeSupport.firePropertyChange("turno", oldTurno, turno);
+    }
+    public Paciente(Integer idpaciente) {
+        this.idpaciente = idpaciente;
+    }
+
+    public Integer getIdpaciente() {
+        return idpaciente;
+    }
+
+    public void setIdpaciente(Integer idpaciente) {
+        Integer oldIdpaciente = this.idpaciente;
+        this.idpaciente = idpaciente;
+        changeSupport.firePropertyChange("idpaciente", oldIdpaciente, idpaciente);
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        String oldBairro = this.bairro;
+        this.bairro = bairro;
+        changeSupport.firePropertyChange("bairro", oldBairro, bairro);
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        String oldCep = this.cep;
+        this.cep = cep;
+        changeSupport.firePropertyChange("cep", oldCep, cep);
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        String oldCpf = this.cpf;
+        this.cpf = cpf;
+        changeSupport.firePropertyChange("cpf", oldCpf, cpf);
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        Date oldDataNascimento = this.dataNascimento;
+        this.dataNascimento = dataNascimento;
+        changeSupport.firePropertyChange("dataNascimento", oldDataNascimento, dataNascimento);
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        String oldFoto = this.foto;
+        this.foto = foto;
+        changeSupport.firePropertyChange("foto", oldFoto, foto);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        String oldNome = this.nome;
+        this.nome = nome;
+        changeSupport.firePropertyChange("nome", oldNome, nome);
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String número) {
+        String oldNúmero = this.numero;
+        this.numero = número;
+        changeSupport.firePropertyChange("numero", oldNúmero, número);
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        String oldRua = this.rua;
+        this.rua = rua;
+        changeSupport.firePropertyChange("rua", oldRua, rua);
+    }
+
+   public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        Cidade oldCidade = this.cidade;
+        this.cidade = cidade;
+        changeSupport.firePropertyChange("cidade", oldCidade, cidade);
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        Estado oldEstado = this.estado;
+        this.estado = estado;
+        changeSupport.firePropertyChange("estado", oldEstado, estado);
+    }
+
+    public Profissao getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(Profissao profissao) {
+        Profissao oldProfissao = this.profissao;
+        this.profissao = profissao;
+        changeSupport.firePropertyChange("profissao", oldProfissao, profissao);
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        String oldApelido = this.apelido;
+        this.apelido = apelido;
+        changeSupport.firePropertyChange("apelido", oldApelido, apelido);
+        
+    }
+
+    
+
+
+    
+    
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idpaciente != null ? idpaciente.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Paciente)) {
+            return false;
+        }
+        Paciente other = (Paciente) object;
+        if ((this.idpaciente == null && other.idpaciente != null) || (this.idpaciente != null && !this.idpaciente.equals(other.idpaciente))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
+    }
+    
+}
